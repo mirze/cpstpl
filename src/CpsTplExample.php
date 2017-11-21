956082459c05861da25c71746474f65172feff9c
magina <magina@maginas-MacBook-Pro.local>
2017-11-21 17:31:06 +0800

    add the file named phpunit.xml

---
+++ src/CpsTplExample.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Yin32167\CpsTpl;
+
+
+class CpsTplExample {
+
+    public function show() {
+
+        return "Hi I'm magina, this is my template of composer project!";
+    }
+}
---
+++ tests/CpsTplExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yin32167\CpsTpl\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yin32167\CpsTpl\CpsTplExample;
+
+final class CpsTplExampleTest extends TestCase 
+{
+
+    public function testCannotBeShow(): void
+    {
+        $cpstpl_example = new CpsTplExample;
+        
+        $this->expectOutputString($cpstpl_example->show());
+        print $cpstpl_example->show();
+    }
+
+}
